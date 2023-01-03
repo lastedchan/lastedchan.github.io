9387cff995f6573709701630a4269f8470b69925
lastedchan <lastedchan@gmail.com>
2023-01-03 17:59:12 +0900

    coreCalc 제작중

--- components/maplestory/vMatrixCalculator/vMatrixCalculator.tsx
+++ components/maplestory/vMatrixCalculator/vMatrixCalculator.tsx
@@ -13,7 +13,7 @@ export default function VMatrixCalculator() {
   const [job, setJob] = useState<string>(
     localStorage.getItem(COOKIE_PREFIX_V_MATRIX_CALCULATOR + "job") ?? ""
   );
-  const [slotCount, setSlotCount] = useState<number>();
+  const [slotCount, setSlotCount] = useState<number>(0);
   const [coreStackList, setCoreStackList] = useState<CoreStackListType>({});
   const [myCoreList, setMyCoreList] = useState<string[][]>([]);
 
@@ -24,13 +24,13 @@ export default function VMatrixCalculator() {
         Number(
           localStorage.getItem(
             COOKIE_PREFIX_V_MATRIX_CALCULATOR + "slot_count"
-          ) ?? 0
+          ) ?? 1
         )
       ),
     []
   );
   useEffect(() => {
-    if (slotCount !== undefined) {
+    if (slotCount) {
       localStorage.setItem(
         COOKIE_PREFIX_V_MATRIX_CALCULATOR + "slot_count",
         String(slotCount)
@@ -45,9 +45,11 @@ export default function VMatrixCalculator() {
         <TextField
           type={"number"}
           inputMode={"numeric"}
-          label={"코어 수"}
+          label={"최대 코어 수"}
           value={slotCount ?? ""}
-          onChange={e => setSlotCount(Number(e.target.value))}
+          onChange={e =>
+            Number(e.target.value) > 0 && setSlotCount(Number(e.target.value))
+          }
           sx={{ flex: "0 0 120px", input: { textAlign: "right" } }}
         />
       </Box>
@@ -67,9 +69,46 @@ export default function VMatrixCalculator() {
         myCoreList={myCoreList}
         setMyCoreList={setMyCoreList}
       />
-      <Button variant={"contained"} color={"secondary"} sx={{ flex: 0 }}>
+      <Button
+        variant={"contained"}
+        color={"secondary"}
+        sx={{ flex: 0 }}
+        onClick={() =>
+          console.log(coreCalc(coreStackList, myCoreList, slotCount))
+        }
+      >
         계산
       </Button>
     </>
   );
+}
+
+function coreCalc(
+  coreStackList: CoreStackListType,
+  myCoreList: string[][],
+  coreCount: number,
+  resultCoreList: string[][] = [],
+  currDepth: number = 1
+): string[][] | false {
+  const core = myCoreList.shift();
+  if (core) {
+    resultCoreList.push(core);
+    resultCoreList.forEach(_ => _.forEach(__ => coreStackList[__]--));
+    const success = !Object.values(coreStackList).find(_ => _ > 0);
+    if (success) {
+      return resultCoreList;
+    } else if (currDepth < coreCount) {
+      return coreCalc(
+        coreStackList,
+        myCoreList.filter(_ => _[0] !== core[0]),
+        coreCount,
+        resultCoreList,
+        currDepth + 1
+      );
+    } else {
+      return false;
+    }
+  } else {
+    return resultCoreList;
+  }
 }
