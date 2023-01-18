373c6df3d17af29d879598f4a5bf68055458fceb
lastedchan <lastedchan@gmail.com>
2023-01-18 11:15:55 +0900

    탭 메뉴 순서 변경

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -6,12 +6,12 @@ export const tabList: TabListType = [
   { title: "HOME", href: "/", show: true },
   null,
   { title: "MapleStory", show: true },
-  { title: "미드나잇 체이서", href: "/midnight_chaser", show: true },
   {
     title: "V 매트릭스 계산기",
     href: "/v_matrix_calculator",
     show: true,
   },
+  { title: "미드나잇 체이서", href: "/midnight_chaser", show: true },
 ];
 
 export const V_MATRIX_CALCULATOR_PREFIX = "v_matrix_calculator_";
