8e6f8411970efa5d9173985cbdefb231e9b6ea5e
lastedchan <lastedchan@gmail.com>
2023-01-18 10:58:27 +0900

    dev 모드일때 gtag 작동 안하도록

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -57,40 +57,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <RecoilRoot>
-      <Script
-        id={"gtm"}
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-P7TNQRK');
-      `,
-        }}
-      />
-      {/* GA 설정 시작 */}
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${gtag.GA_TRACKING_ID}', {
-          page_path: window.location.pathname,
-        });
-      `,
-        }}
-      />
-      {/* GA 설정 끝 */}
+      <Gtm />
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8583770780355894"
@@ -131,3 +98,43 @@ const Wrapper = styled(Box)`
   height: 100%;
   overflow: auto;
 `;
+
+const Gtm = () =>
+  process.env.NODE_ENV !== "development" ? (
+    <>
+      <Script
+        id={"gtm"}
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P7TNQRK');
+          `,
+        }}
+      />
+      {/* GA 설정 시작 */}
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      {/* GA 설정 끝 */}
+    </>
+  ) : null;
--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -2,16 +2,20 @@ export const GA_TRACKING_ID = "G-LZLFJGKZJT"; // 측정ID 설정: .env 파일로
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = url => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (process.env.NODE_ENV !== "development") {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category = "", label = "", value = "" }) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  if (process.env.NODE_ENV !== "development") {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
 };
