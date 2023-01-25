2c2461aeb7a60eb41583752060d2fc5ad4fc6e1b
lastedchan <lastedchan@gmail.com>
2023-01-25 16:24:55 +0900

    nav 에서 미드나잇 체이서 도우미 제거

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -8,7 +8,7 @@ export const tabList: TabListType = [
   { title: "MapleStory", show: true },
   { title: "V 매트릭스 계산기", href: "/v_matrix_calculator", show: true },
   { title: "결정석 수익 계산기", href: "/crystal_calculator", show: true },
-  { title: "미드나잇 체이서 도우미", href: "/midnight_chaser", show: true },
+  { title: "미드나잇 체이서 도우미", href: "/midnight_chaser", show: false },
 ];
 
 export const V_MATRIX_CALCULATOR_PREFIX = "v_matrix_calculator_";
