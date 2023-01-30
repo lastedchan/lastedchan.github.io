d43e36cc3a775337b4acea74afe3031e626349c6
lastedchan <lastedchan@gmail.com>
2023-01-30 15:47:52 +0900

    메뉴에 제작자 인게임 프로필 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatar.maplestory.nexon.com",
+        port: "",
+        pathname: "/Character/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ssl.nexon.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/constants/common.tsx
+++ src/constants/common.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import VMatrixCalculatorHelp from "../components/maplestory/vMatrixCalculator/vMatrixCalculatorHelp";
 import * as gtag from "../lib/gtag";
+import { Box, Typography } from "@mui/material";
 
 export const topTitle = "LASTCHAN";
 
@@ -22,7 +23,7 @@ export const tabList: TabListType = [
   { title: "문의 및 건의사항", show: true },
   {
     title: (
-      <span style={{ display: "block", margin: "auto", width: 150 }}>
+      <Box margin={"auto"} width={150}>
         <Link
           href={"https://open.kakao.com/o/stON1QZe"}
           target={"_blank"}
@@ -30,7 +31,47 @@ export const tabList: TabListType = [
         >
           <Image src={"/qr.png"} alt={"QR code"} width={600} height={600} />
         </Link>
-      </span>
+      </Box>
+    ),
+    show: true,
+  },
+  {
+    title: (
+      <Box margin={"auto"} width={150} textAlign={"center"}>
+        <Typography fontWeight={"bold"}>제작자</Typography>
+        <Link
+          href={"https://maple.gg/u/%EC%AB%91%EC%B0%AE"}
+          target={"_blank"}
+          onClick={() => gtag.event({ action: "maplegg_click" })}
+        >
+          <Image
+            src={
+              "https://avatar.maplestory.nexon.com/Character/POKGLPEJLHKIGJGHDDOGAOBLOOIBICLBHFDPPEFAFCLGGPLNNCALOOHKEFGFOLIBIOEJLMNIKAODJHDEOFBIBGOGJMJKMGICBINIBJHADDFJFKGOMNPBKIELGGIBBDGIFKIBCPDBLCLPKKDLEKNKCAJIGJELMKNKENEFINICIICFHGIKBFKPMMFMNPNDIHCBDNDOHJOHDMPEFEHHPAIIFKHHHIOFNBADPGKMGDGEANJAKFPJNPCKNFBCPLBIBAAP.png"
+            }
+            alt={"쫑찮"}
+            width={168}
+            height={168}
+          />
+        </Link>
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          gap={1}
+        >
+          <Typography style={{ display: "block", width: 14, height: 14 }}>
+            <Image
+              src={
+                "https://ssl.nexon.com/s2/game/maplestory/renewal/common/world_icon/icon_3.png"
+              }
+              alt={""}
+              width={14}
+              height={14}
+            />
+          </Typography>
+          <Typography>쫑찮</Typography>
+        </Box>
+      </Box>
     ),
     show: true,
   },
