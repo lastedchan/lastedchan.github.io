f1fa22cd895f07574b262e94bcfb06a806367875
lastedchan <lastedchan@gmail.com>
2023-03-02 15:59:02 +0900

    애드센스 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,14 +9,16 @@ import { RecoilRoot } from "recoil";
 import * as gtag from "../src/libs/gtag";
 import App_ from "../src/components/layouts/app";
 import Gtm from "../src/libs/gtm";
-import Script from "next/script";
 import Head from "next/head";
 import { MAIN_TITLE, TAB_LIST } from "../src/constants/common";
 
 export default function App({ Component, pageProps, router: r }: AppProps) {
   const router = useRouter();
 
-  const subtitle = useMemo(() => TAB_LIST.find(_ => _?.href === router.pathname)?.title, [router.pathname]);
+  const subtitle = useMemo(
+    () => TAB_LIST.find((_) => _?.href === router.pathname)?.title,
+    [router.pathname]
+  );
 
   useEffect(() => {
     const handleRouteChange = (url: any) => {
@@ -33,13 +35,11 @@ export default function App({ Component, pageProps, router: r }: AppProps) {
   return (
     <RecoilRoot>
       <Gtm />
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8583770780355894"
-        crossOrigin="anonymous"
-      />
       <Head>
-        <meta name={"viewport"} content={"width=device-width,initial-scale=1"} />
+        <meta
+          name={"viewport"}
+          content={"width=device-width,initial-scale=1"}
+        />
         <meta name={"title"} content={MAIN_TITLE + " - " + subtitle} />
         <title>{MAIN_TITLE + " - " + subtitle}</title>
       </Head>
